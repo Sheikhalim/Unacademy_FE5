cdc95cbcf011844b42cbdef2ca3403f580ccb1d5
Sheikhalim <97816133+Sheikhalim@users.noreply.github.com>
2022-06-02 09:49:21 +0530

    Update object.js

--- Class-01-OOPs/object.js
+++ Class-01-OOPs/object.js
@@ -5,15 +5,16 @@ class Blueprint{
     kitchen = 1;
 
     // function called constructor
-    constructor(noOfBedRooms,noOfKitchen){
+    constructor(noOfBedRooms,noOfKitchen,noOfHall){
         this.bedRoom = noOfBedRooms;
         this.kitchen = noOfKitchen;
+        this.hall=noOfHall;
     }
 }
 
-let house1 = new Blueprint(4,1);
-let house2 = new Blueprint(3,1);
-let house3 = new Blueprint(2,3);
+let house1 = new Blueprint(4,1,1);
+let house2 = new Blueprint(3,1,2);
+let house3 = new Blueprint(2,3,1);
 
 
 console.log(house1)
